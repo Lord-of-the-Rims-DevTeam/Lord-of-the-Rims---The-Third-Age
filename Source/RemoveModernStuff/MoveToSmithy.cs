b34eebc526e998ee772bcc63ee6fe549944462b1
jecrell <matt.walls31@gmail.com>
2018-04-23 02:44:46 +0900

    Initial commit

---
+++ Source/RemoveModernStuff/MoveToSmithy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace RemoveModernStuff
+{
+    [StaticConstructorOnStartup]
+    public static class MoveToSmithy
+    {
+        private static int movedDefs;
+        private static int steelDefs;
+        static MoveToSmithy()
+        {
+            MoveRecipesToSmithy();
+
+            ChangeSteelToIron();
+        }
+
+        private static void MoveRecipesToSmithy()
+        {
+            movedDefs = 0;
+            foreach (ThingDef td in DefDatabase<ThingDef>.AllDefs.Where(t =>
+                (t?.recipeMaker?.recipeUsers?.Contains(ThingDef.Named("FueledSmithy")) ?? false) ||
+                (t?.recipeMaker?.recipeUsers?.Contains(ThingDef.Named("TableMachining")) ?? false)))
+            {
+                td.recipeMaker.recipeUsers.RemoveAll(x => x.defName == "TableMachining" ||
+                                                          x.defName == "FueledSmithy");
+                td.recipeMaker.recipeUsers.Add(ThingDef.Named("LotR_TableSmithy"));
+                movedDefs++;
+            }
+            Log.Message("Moved " + movedDefs + " from Machining Table to Smithy.");
+        }
+
+        private static void ChangeSteelToIron()
+        {
+            steelDefs = 0;
+            foreach (ThingDef tdd in DefDatabase<ThingDef>.AllDefs.Where(tt =>
+                tt?.costList?.Any(y => y?.thingDef == ThingDefOf.Steel) ?? false))
+            {
+                var tempCost = tdd.costList.FirstOrDefault(z => z.thingDef == ThingDefOf.Steel);
+                var newTempCost = new ThingCountClass(ThingDef.Named("LotR_Iron"), tempCost.count);
+                tdd.costList.Remove(tempCost);
+                tdd.costList.Add(newTempCost);
+                steelDefs++;
+            }
+            if (ThingDefOf.Steel?.deepCommonality > 2)
+                ThingDefOf.Steel.deepCommonality = 2;
+            Log.Message("Replaced " + steelDefs + " defs with Iron.");
+        }
+    }
+}
---
+++ Source/RemoveModernStuff/RemoveModernStuff.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using RimWorld;
+using RimWorld.BaseGen;
+using Verse;
+
+namespace RemoveModernStuff
+{
+    [StaticConstructorOnStartup]
+    public static class RemoveModernStuff
+    {
+        const TechLevel maxTechLevel = TechLevel.Medieval;
+        static int removedDefs;
+
+        static RemoveModernStuff()
+        {
+            removedDefs = 0;
+            IEnumerable<ResearchProjectDef> projects = DefDatabase<ResearchProjectDef>.AllDefs.Where(rpd => rpd.techLevel > maxTechLevel);
+            IEnumerable<ThingDef> things = DefDatabase<ThingDef>.AllDefs.Where(td => td.techLevel > maxTechLevel || (td.researchPrerequisites?.Any(rpd => projects.Contains(rpd)) ?? false) || td.defName == "Gun_Revolver");
+            
+            RemoveStuff(typeof(DefDatabase<RecipeDef>), DefDatabase<RecipeDef>.AllDefs.Where(rd => rd.products.Any(tcc => things.Contains(tcc.thingDef)) || rd.AllRecipeUsers.All(td => things.Contains(td)) || projects.Contains(rd.researchPrerequisite)).Cast<Def>());
+            RemoveStuff(typeof(DefDatabase<ResearchProjectDef>), projects.Cast<Def>());
+
+            FieldInfo getThingInfo = typeof(ScenPart_ThingCount).GetField("thingDef", BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (ScenarioDef def in DefDatabase<ScenarioDef>.AllDefs)
+                foreach (ScenPart sp in def.scenario.AllParts)
+                    if (typeof(ScenPart_ThingCount).IsAssignableFrom(sp.GetType()) && things.Contains((ThingDef) getThingInfo.GetValue(sp)))
+                    {
+                        def.scenario.RemovePart(sp);
+                    }
+
+            RemoveStuff(typeof(DefDatabase<ThingDef>), things.Cast<Def>());
+
+            MethodInfo resolveDesignatorsAgain = typeof(DesignationCategoryDef).GetMethod("ResolveDesignators", BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (DesignationCategoryDef dcd in DefDatabase<DesignationCategoryDef>.AllDefs)
+                resolveDesignatorsAgain.Invoke(dcd, null);
+
+            RemoveStuff(typeof(DefDatabase<PawnKindDef>), DefDatabase<PawnKindDef>.AllDefs.Where(pkd => pkd.defaultFactionType != FactionDefOf.PlayerColony && (pkd.race.techLevel > maxTechLevel || pkd.defaultFactionType?.techLevel > maxTechLevel)).Cast<Def>());
+            RemoveStuff(typeof(DefDatabase<FactionDef>), DefDatabase<FactionDef>.AllDefs.Where(fd => fd.techLevel > maxTechLevel).Cast<Def>());
+
+            foreach (MapGeneratorDef mgd in DefDatabase<MapGeneratorDef>.AllDefs)
+                mgd.GenSteps.RemoveAll(gs => gs.genStep is GenStep_SleepingMechanoids || gs.genStep is GenStep_Turrets || gs.genStep is GenStep_Power);
+
+            foreach (RuleDef rd in DefDatabase<RuleDef>.AllDefs)
+            {
+                rd.resolvers.RemoveAll(sr => sr is SymbolResolver_AncientCryptosleepCasket || sr is SymbolResolver_ChargeBatteries || sr is SymbolResolver_EdgeMannedMortar || sr is SymbolResolver_FirefoamPopper || sr is SymbolResolver_MannedMortar || sr is SymbolResolver_OutdoorLighting);
+                if (rd.resolvers.Count == 0)
+                    rd.resolvers.Add(new SymbolResolver_AddWortToFermentingBarrels());
+            }
+
+            Log.Message("Removed " + removedDefs + " modern defs");
+            
+        }
+
+        static void RemoveStuff(Type databaseType, IEnumerable<Def> defs)
+        {
+            MethodInfo mi = databaseType.GetMethod("Remove", BindingFlags.Static | BindingFlags.NonPublic);
+            while(defs.Any())
+            {
+                removedDefs++;
+                mi.Invoke(null, new object[] { defs.First() });
+            }
+        }
+    }
+}
